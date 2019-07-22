b72d7928f875aac89bc4fbd67ac5173e98db5a9d
LaszloNagy85 <n.laszlo.uk@gmail.com>
2019-07-22 14:41:07 +0200

    Add data selector function.

---
+++ data_manager.py
@@ -0,0 +1,13 @@
+import connection
+
+
+def get_all_data(file_name):
+    return connection.get_all_data_from_file(file_name)
+
+
+SELECTED_DATA = 0
+
+
+def get_selected_data(file_name, data_id):
+    existing_data = connection.get_all_data_from_file(file_name)
+    return [row for row in existing_data if row['id'] == data_id][SELECTED_DATA]
