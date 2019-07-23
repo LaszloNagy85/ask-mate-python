c7b3953d8732fdc0a6ba22716242e2b84c68e20c
wory04 <voroseszterveronika@gmail.com>
2019-07-23 18:36:44 +0200

    implement route_new_answer function

--- server.py
+++ server.py
@@ -1,21 +1,42 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 import data_manager
+import util
 
 app = Flask(__name__)
 
 
 @app.route('/question/<question_id>')
-def route_question(question_id=None):
-    question = data_manager.get_selected_data('question', question_id, 'id')[0]
+def route_question(question_id):
+    question = data_manager.get_selected_data('question', question_id, 'id')  # note to self: reconsider this!
     answers = data_manager.get_selected_data('answer', question_id, 'question_id')
-    print(question)
-    print(answers)
+
+    print(question)  # for testing, delete it later!
+    print(answers)  # for testing, delete it later!
 
     return render_template('question.html',
                            question=question,
                            answers=answers,
                            )
+
+
+@app.route('/question/<question_id>/new-answer', methods=['POST'])
+def route_new_answer(question_id):
+    new_answer_id = data_manager.generate_id('answer')
+    submission_time = util.get_epoch()
+
+    modified_answer = {
+        'id': new_answer_id,
+        'submission_time': submission_time,
+        'vote_number': '0',
+        'question_id': question_id,
+        'message': request.form.get('message'),
+        'image': '',  # will be modified later
+    }
+
+    print(modified_answer)  # just for check until there is an export data function
+    #  call export data function comes here
+    return redirect(f'/question/{question_id}')
 
 
 if __name__ == '__main__':
