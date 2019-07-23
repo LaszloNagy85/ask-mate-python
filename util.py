012d7dad00824519468ffccb7ca614cce9590473
wory04 <voroseszterveronika@gmail.com>
2019-07-23 18:39:53 +0200

    add functions for date handling: get_epoch, convert_epoch_to_readable

--- util.py
+++ util.py
@@ -1,6 +1,17 @@
+import time
+
+
 def get_id(existing_data, change=1):
 
     if len(existing_data) == 0:
         return '1'
 
     return str(int(existing_data[-1]['id']) + change)
+
+
+def get_epoch():
+    return str(int(time.time()))
+
+
+def convert_epoch_to_readable(time_epoch):
+    return time.strftime('%Y. %m. %d. %H:%M:%S', time.localtime(int(time_epoch)))
