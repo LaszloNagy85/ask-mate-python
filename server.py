e4c5a1b81ad4b74aa42618eec96dc2b270d02190
wory04 <voroseszterveronika@gmail.com>
2019-07-23 14:58:32 +0200

    implement show question and answers

--- server.py
+++ server.py
@@ -5,11 +5,17 @@ import data_manager
 app = Flask(__name__)
 
 
+@app.route('/question/<question_id>')
+def route_question(question_id=None):
+    question = data_manager.get_selected_data('question', question_id, 'id')[0]
+    answers = data_manager.get_selected_data('answer', question_id, 'question_id')
+    print(question)
+    print(answers)
 
-
-
-
-
+    return render_template('question.html',
+                           question=question,
+                           answers=answers,
+                           )
 
 
 if __name__ == '__main__':
