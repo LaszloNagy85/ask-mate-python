0c6f24c1156b2d6625c3eb90e1aefe024ae48801
wory04 <voroseszterveronika@gmail.com>
2019-07-24 20:07:23 +0200

    fix not oploading option for new answer

--- server.py
+++ server.py
@@ -37,8 +37,8 @@ def route_new_answer(question_id):
     submission_time = util.get_epoch()
     if 'image' in request.files:
         image = request.files['image']
-        print(image)
-        image.save(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
+        if image.filename != "":
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
     else:
         image = ''
 
@@ -48,7 +48,7 @@ def route_new_answer(question_id):
         'vote_number': '0',
         'question_id': question_id,
         'message': request.form.get('message'),
-        'image': image if not image else image.filename,
+        'image': image.filename if image else None,
     }
 
     data_manager.add_data(new_answer, 'answer', DATA_HEADER_ANSWER)
