737a38b3813e5f093166cca9a08b3c74f1e95388
wory04 <voroseszterveronika@gmail.com>
2019-08-06 17:48:10 +0200

    add comments to share tasks between teammates part 2

--- data_manager.py
+++ data_manager.py
@@ -3,16 +3,16 @@ import database_connection
 import util
 
 
-def get_all_data(file_name):
+def get_all_data(file_name):  # Z
     return connection.get_all_data_from_file(file_name)
 
 
-def get_selected_data(file_name, data_id, data_key):
+def get_selected_data(file_name, data_id, data_key):  # N (2 kell: fetchone, fetchall)
     existing_data = connection.get_all_data_from_file(file_name)
     return [row for row in existing_data if row[data_key] == data_id]
 
 
-def get_all_data_of_one_type(type_, file_name):
+def get_all_data_of_one_type(type_, file_name):  # do we need this?
     existing_data = connection.get_all_data_from_file(file_name)
     selected_data = []
     for row in existing_data:
