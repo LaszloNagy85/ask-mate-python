4022686ee6847ea35557eaabc6961a37a23b0c3d
wory04 <voroseszterveronika@gmail.com>
2019-08-06 17:14:06 +0200

    add comments to mark tasks for team members

--- data_manager.py
+++ data_manager.py
@@ -3,16 +3,16 @@ import database_connection
 import util
 
 
-def get_all_data(file_name):
+def get_all_data(file_name):  # Z
     return connection.get_all_data_from_file(file_name)
 
 
-def get_selected_data(file_name, data_id, data_key):
+def get_selected_data(file_name, data_id, data_key):  # N (2 kell: fetchone, fetchall)
     existing_data = connection.get_all_data_from_file(file_name)
     return [row for row in existing_data if row[data_key] == data_id]
 
 
-def get_all_data_of_one_type(type_, file_name):
+def get_all_data_of_one_type(type_, file_name):  # do we need this?
     existing_data = connection.get_all_data_from_file(file_name)
     selected_data = []
     for row in existing_data:
@@ -20,17 +20,17 @@ def get_all_data_of_one_type(type_, file_name):
     return selected_data
 
 
-def generate_id(file_name):
+def generate_id(file_name):  # delete
     all_data = connection.get_all_data_from_file(file_name)
     return util.get_id(all_data)
 
 
-def convert_readable_dates(data):
+def convert_readable_dates(data):  # delete
     for row in data:
         row['submission_time'] = util.convert_epoch_to_readable(row['submission_time'])
 
 
-def delete_answer(id_, id_key):
+def delete_answer(id_, id_key):  # E
     remaining_answers = []
     all_answers = get_all_data('answer')
     for answer in all_answers:
@@ -40,7 +40,7 @@ def delete_answer(id_, id_key):
     connection.write_remaining_data_to_file(remaining_answers, 'answer', connection.DATA_HEADER_ANSWER)
 
 
-def delete_question(id_):
+def delete_question(id_):  # E
     remaining_questions = []
     all_questions = get_all_data('question')
     for question in all_questions:
@@ -51,22 +51,22 @@ def delete_question(id_):
     connection.write_remaining_data_to_file(remaining_questions, 'question', connection.DATA_HEADER_QUESTION)
 
 
-def get_dict_of_specific_types(list_of_types, data):
+def get_dict_of_specific_types(list_of_types, data):  # Z
     specific_data = []
     for row in data:
         specific_data.append({key: value for key, value in row.items() if key in list_of_types})
     return specific_data
 
 
-def add_data(data, file_name, data_header):
+def add_data(data, file_name, data_header):  # Z
     return connection.write_data_to_file(data, file_name, data_header)
 
 
-def update_data(data, file_name, data_header):
+def update_data(data, file_name, data_header):  # Z
     return connection.write_data_to_file(data, file_name, data_header, False)
 
 
-def get_sorted_data(file_name, sort_by, direction):
+def get_sorted_data(file_name, sort_by, direction):  # N
     data_to_sort = get_all_data(file_name)
     if sort_by == 'title':
         is_int = str
@@ -80,7 +80,7 @@ def get_sorted_data(file_name, sort_by, direction):
     return sorted_data
 
 
-def save_vote(file_name, question_id, vote_type, data_header, answer_id):
+def save_vote(file_name, question_id, vote_type, data_header, answer_id):  # Z
     existing_votes = connection.get_all_data_from_file(file_name)
     voted_id = question_id if answer_id == 'None' else answer_id
     for row in existing_votes:
